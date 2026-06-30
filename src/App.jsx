0b32374532b2d8d5303e1c096361409c67e9af6f
lum-i3 <dana.bahena.21@gmail.com>
2026-06-30 12:25:33 -0600

    se agrego la pantalla de perfil

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from 'react';
 import Login from './pages/Login';
 import EditarPerfil from './pages/usuario/EditarPerfil';
 import SidebarLayout from './components/SidebarLayout';
+import Perfil from './pages/usuario/Perfil';
 
 function App() {
 
   return (
     <div>      
-        <SidebarLayout/>
+        <Perfil/>
     </div>
   );
 }
--- src/pages/usuario/EditarPerfil.jsx
+++ src/pages/usuario/EditarPerfil.jsx
@@ -4,6 +4,15 @@ import SidebarLayout from "../../components/SidebarLayout";
 function EditarPerfil() {
   return (
     <SidebarLayout>
+      <Card className="m-4">
+        <Card.Body>
+          <Row>
+            <Col>
+              <h3>Editar Perfil</h3>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
     </SidebarLayout>
   );
 }
